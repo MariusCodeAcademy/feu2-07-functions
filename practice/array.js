6a67d25d15cad1bad7127e4d4cc19fe7e1b4dc8c
Marius <marius@example.com>
2022-10-18 16:03:10 +0300

    calculate

--- practice/array.js
+++ practice/array.js
@@ -44,3 +44,55 @@ console.log('rezultatas ===', rezultatas);
 
 rezultatas = makeCurrency(true);
 console.log('rezultatas ===', rezultatas);
+
+// parasyti funkcija kuri grazina maziausia is dvieju skaiciu
+
+function min(n1, n2) {
+  //   5   7
+  //   7   5
+  // if (n1 < n2) {
+  //   return n1;
+  // }
+  // return n2;
+
+  // n1 < n2 ? return n1 :  return n2;
+  // ternary operatorius su grazinimu
+  return n1 < n2 ? n1 : n2;
+}
+
+const minAn = (n1, n2) => (n1 < n2 ? n1 : n2);
+
+// console.log('min(5, 7) ===', min(5, -7));
+// console.log('min(7, 5) ===', min(7, -5));
+// console.log('minAn(5, 7) ===', minAn(5, -7));
+
+// 4. skaidriu. Parašykite funkciją calculate(num1, num2, action), kuri atliks action parametru nurodytą matematinį veiksmą (“composition”, “subtraction”, “multiplication”, “division”) su num1 ir num2 nurodytais skaičiais.
+
+// calculate(8, 9, 'plus') // 17
+// calculate(8, 9, 'minus') // 17
+// calculate(8, 9, 'multi') // 17
+
+function calculate(num1, num2, operation) {
+  // norim pasakyti kad galimo reiksmes yra tik
+  if (!['plus', 'minus', 'multi'].includes(operation)) {
+    return 'bad operation';
+  }
+  //
+  if (operation === 'plus')
+    // calculate(8, 9, 'plus')
+    return num1 + num2;
+  // calculate(8, 9, 'minus')
+  if (operation === 'minus') return num1 - num2;
+  // calculate(8, 9, 'multi')
+  if (operation === 'multi') return num1 * num2;
+
+  // kam cia 'operation' nera lygus ?
+  return `"${operation}" what ta hell is this? neiteisinga operacija, please check the manual!!!!`;
+}
+// issaugom grazinama calculate reiksme i sum11
+let sum11 = calculate(8, 9, 'plus');
+console.log('sum11 ===', sum11);
+let minus11 = calculate(8, 9, 'minus');
+console.log('minus11 ===', minus11);
+console.log("calculate(8, 9, 'multi') ===", calculate(8, 9, 'multi'));
+console.log("calculate(8, 9, 'expo300') ===", calculate(8, 9, 'expo300'));
